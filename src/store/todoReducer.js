891deadeecdd800b2f6357b435c6de3a8a6967f5
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-05 22:11:52 +0000

    Refactor reducer into two separate functions

--- src/store/todoReducer.js
+++ src/store/todoReducer.js
@@ -1,24 +1,39 @@
+// Update a single todo:
+
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed
+      };
+    default:
+      return state;
+  }
+}
+
+// Manage the array of todos:
+// Reducers are normal js functions so you can have many reducers calling
+// eachother and each contributing to a part of the application state tree.
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo;
-        }
-        return {
-          ...todo,
-          completed: !todo.completed
-        };
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
