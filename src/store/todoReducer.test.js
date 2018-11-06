9d634639a64b03641d3bf0e6e472d26727c804b6
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-06 22:15:44 +0000

    Update tests for combined reducer object

--- src/store/todoReducer.test.js
+++ src/store/todoReducer.test.js
@@ -1,54 +1,66 @@
-const todos = require('./todoReducer');
+const todoApp = require('./todoReducer');
 
 describe('To do reducer', () => {
   it('correctly adds a todo to the state', () => {
-    const stateBefore = [];
+    const stateBefore = {
+      todos: [],
+      visibilityFilter: "SHOW_ALL"
+    };
     const action = {
       type: 'ADD_TODO',
       id: 0,
       text: 'Learn Redux'
     };
-    const stateAfter = [
-      {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-      }
-    ]
-  expect(todos(stateBefore, action)).toEqual(stateAfter);
+    const stateAfter = {
+      todos: [
+        {
+          id: 0,
+          text: 'Learn Redux',
+          completed: false
+        }
+      ],
+      visibilityFilter: "SHOW_ALL"
+    };
+  expect(todoApp(stateBefore, action)).toEqual(stateAfter);
   });
 
   it('toggles the todo state', () => {
-    const stateBefore = [
-      {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-      },
-      {
-        id: 1,
-        text: 'Buy some lurex trousers',
-        completed: false
-      }
-    ];
+    const stateBefore = {
+      todos: [
+        {
+          id: 0,
+          text: 'Learn Redux',
+          completed: false
+        },
+        {
+          id: 1,
+          text: 'Buy some lurex trousers',
+          completed: false
+        }
+      ],
+      visibilityFilter: "SHOW_ALL"
+    };
 
     const action = {
       type: 'TOGGLE_TODO',
       id: 1,
     };
 
-    const stateAfter = [
-      {
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-      },
-      {
-        id: 1,
-        text: 'Buy some lurex trousers',
-        completed: true
-      }
-    ]
-  expect(todos(stateBefore, action)).toEqual(stateAfter);
+    const stateAfter = {
+      todos: [
+        {
+          id: 0,
+          text: 'Learn Redux',
+          completed: false
+        },
+        {
+          id: 1,
+          text: 'Buy some lurex trousers',
+          completed: true
+        }
+      ],
+      visibilityFilter: "SHOW_ALL"
+    };
+  expect(todoApp(stateBefore, action)).toEqual(stateAfter);
   });
 });
