e17cfb6afbdcf8f0fb2e2b05d5f78bd392feca60
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-12 23:04:48 +0000

    Fix Babel setup for React, ES6 and JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 const path = require('path');
+const webpack = require("webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src'),
+  entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              'css-loader'
+            ]
+          },
+          {
+            test: path.join(__dirname, '.'),
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader'
+            }
+          }
+        ]
+    },
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+
   devServer: {
+    contentBase: path.join(__dirname,'public'),
     port: 3000,
-    contentBase: path.resolve(__dirname, 'public')
+    publicPath: "http://localhost:3000/dist"
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  plugins: [
-  ]
-}
+};
