597e1bfb2db5a1da638da2dc63a762b927f42fb2
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-17 14:40:43 +0000

    Comment out hardcoded state changes
    * View layer takes faciliates this via React

--- src/store/store.js
+++ src/store/store.js
@@ -5,48 +5,52 @@ const todoApp = require('./todoReducer');
 const { createStore } = Redux;
 export const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('---------------');
 
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux'
-});
+// Exmaple of hardcoded actions being dispatched and logged
+// This is dispatching and statechange handled by the view layer using react
 
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching ADD_TODO');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go Shopping'
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching TOGGLE_TODO');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED'
-});
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('---------------');
+// console.log('Initial state:');
+// console.log(store.getState());
+// console.log('---------------');
+//
+// console.log('Dispatching ADD_TODO');
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   id: 0,
+//   text: 'Learn Redux'
+// });
+//
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('---------------');
+//
+// console.log('Dispatching ADD_TODO');
+// store.dispatch({
+//   type: 'ADD_TODO',
+//   id: 1,
+//   text: 'Go Shopping'
+// });
+//
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('---------------');
+//
+// console.log('Dispatching TOGGLE_TODO');
+// store.dispatch({
+//   type: 'TOGGLE_TODO',
+//   id: 0
+// });
+//
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('---------------');
+//
+// console.log('Dispatching SET_VISIBILITY_FILTER');
+// store.dispatch({
+//   type: 'SET_VISIBILITY_FILTER',
+//   filter: 'SHOW_COMPLETED'
+// });
+//
+// console.log('Current state:');
+// console.log(store.getState());
+// console.log('---------------');
