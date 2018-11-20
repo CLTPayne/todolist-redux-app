6833411f8ee2928de0f12af7a57e5273a97b906a
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-20 22:31:58 +0000

    Fix store and reducer imports as per ES^ syntax

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import TodoApp from './components/TodoApp';
 import { createStore } from './store/store';
+import { todoApp } = from './todoReducer';
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
