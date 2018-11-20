024c7eb042e8ae5eb327bb926252ee5b9e1bfacc
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-20 21:23:06 +0000

    Refactor app rendering
    * Call render once
    * Remove top level store subscription
    * Remove unused props passed in from store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import TodoApp from './components/TodoApp';
 import { store } from './store/store';
 
-const render = () => {
-  ReactDOM.render(
-    <TodoApp {...store.getState()} />,
-    document.getElementById('root')
-  );
-};
-
-store.subscribe(render);
-render();
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('root')
+);
