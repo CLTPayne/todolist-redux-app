0baa2f723ec9d5cd953877d4b70191af72909eb8
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-11-20 22:26:26 +0000

    Add provider to wrap the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import TodoApp from './components/TodoApp';
-import { store } from './store/store';
+import { createStore } from './store/store';
 
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={createStore(todoApp)}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('root')
 );
